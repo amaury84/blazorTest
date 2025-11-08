fca96f78cefcb19d5e39a6f29080d7672bf3910e
Amaury Mendez <amaury84@gmail.com>
2025-11-07 20:27:05 -0500

    primer commit

---
+++ blazorTesting/Tests.cs
@@ -0,0 +1,14 @@
+﻿using blazorTesting.Data;
+
+namespace blazorTesting;
+
+public class Tests
+{
+    [Test]
+    public void Basic()
+    {
+        Console.WriteLine("Corriendo test en TUnit");
+    }
+
+    
+}
